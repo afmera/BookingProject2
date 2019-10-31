60a095b7358003dab20033232e0a49ca2db8c927
felipe <felipe@gmail.com>
2019-10-30 21:18:05 -0500

    Ya casi

--- app/src/main/java/com/example/bookingproject/utils/DrawerListAdapter.java
+++ app/src/main/java/com/example/bookingproject/utils/DrawerListAdapter.java
@@ -17,13 +17,13 @@ public class DrawerListAdapter extends ArrayAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-        if(convertView == null){
-            LayoutInflater inflater = (LayoutInflater)parent.getContext().
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) parent.getContext().
                     getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.app_bar_menu__principal_, null);
         }
-        TextView textView_nombUsuario=(TextView)convertView.findViewById(R.id.NombreUsuarioTextView);
+        TextView textView_nombUsuario = (TextView) convertView.findViewById(R.id.NombreUsuarioTextView);
         textView_nombUsuario.setText("Desconocido");
         return convertView;
     }
